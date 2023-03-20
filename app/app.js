ffc9be35c4837fee0b1f4cdbc89eded2583f60b8
Julián Rodrigues Velasques <julirodriguesvelasques@gmail.com>
2023-03-20 12:41:26 -0300

    Add files new documents

--- app/app.js
+++ app/app.js
@@ -1,17 +1,12 @@
-//Consumir api por js mediante petición
-
 const API_BASE = "https://rickandmortyapi.com/api";
 const API_CHARACTERS = "https://rickandmortyapi.com/api/character";
 
-//Paso 1: Guardar llamada de API en variable
 const llamadaAApi = fetch(API_CHARACTERS);
 
-//Pasa 2: "esperar" la respuesta de la API y convertirla a JSON
 llamadaAApi
   .then((data) => {
     return data.json();
   })
-  //Pasa 3: En el último then, la data llega formateada lista para usar
   .then((data) => {
     const $container = document.getElementById("container");
     const characters = data.results;
@@ -32,7 +27,6 @@ llamadaAApi
       `;
     }
   })
-  //Pasa 4: En caso de que haya error aparece en el catch
   .catch((err) => {
     console.log(err);
   });
